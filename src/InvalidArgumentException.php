cba4f4bf443f557466afdbbd5ce83e799269d8f7
Thomas Nunninger <thomas.nunninger@scaleupventures.com>
2019-02-19 20:38:37 +0100

    Extended InvalidArgumentException from Assert\InvalidArgumentException.

--- src/InvalidArgumentException.php
+++ src/InvalidArgumentException.php
@@ -12,6 +12,6 @@
 
 namespace ScaleUpStack\Annotations;
 
-final class InvalidArgumentException extends \InvalidArgumentException
+final class InvalidArgumentException extends \ScaleUpStack\Assert\InvalidArgumentException
 {
 }
